71b3a683b82c4d8b5babf3cb82e0b8d386a9e658
Umangkumar Patel <umangkumar.18beceg031@gmail.com>
2022-06-18 12:11:03 +0530

    Exercise 1 (Movies and Customers Tbales)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,47 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies/Random
-        public ActionResult Random()
+        //// GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Naruto" };
+
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer { Name = "Customer 1" },
+        //        new Customer { Name = "Customer 2" }
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
+
+        public ViewResult Index()
         {
-            var movie = new Movie() { Name = "Naruto" };
-            return View(movie);
+
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Movie 1" },
+                new Movie { Id = 2, Name = "Movie 2" }
+            };
         }
     }
 }
